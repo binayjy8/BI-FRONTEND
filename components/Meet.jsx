7ca4ed064ffaeabe61122865ee3bea951bc6e310
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-22 20:03:26 +0530

    frontend part

--- components/Meet.jsx
+++ components/Meet.jsx
@@ -1,2 +1,6 @@
 import { useState } from "react";
 import useFetch from "../useFetch";
+
+const Meet = () => {
+    const { data, loading, error } = useFetch("https://bi-backend-beige.vercel.app/events");
+}
