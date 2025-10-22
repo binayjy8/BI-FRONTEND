2d73e9d43a847aad93d8587e0433bdae6187dc5a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-22 20:30:03 +0530

    frontend part

--- components/Meet.jsx
+++ components/Meet.jsx
@@ -9,7 +9,7 @@ const Meet = () => {
     <div>
         <h2>All Events</h2>
         <ul>
-            {data?.map((event) => (
+            {data?.event.map((event) => (
                 <li key={event._id}>{event.title}</li>
             ))}
         </ul>
