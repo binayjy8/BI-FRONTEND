4b1d8b6305aa7f57fb74181c98bd0f3d327cce9d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-25 11:40:08 +0530

    Frontend part

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Meet from "../components/Meet";
+import EventDetails from "../components/EventDetails";  
 import useFetch from "../useFetch";
 
 export default function App() {
   return(<main>
     <Meet />
+    <EventDetails />
   </main>)
 }
